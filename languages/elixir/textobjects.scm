ef38a5a8704534f63ab325c9a08612b25df7c0ab
AltCode <12354672+AlternateRT@users.noreply.github.com>
2026-01-31 13:04:24 +0100

    Improve Elixir text objects
    - Adjust anonymous function definitions to account for multiple bodies
    - Adjust function definitions to account for bodiless variants
    - Include function definitions from `deftransform` and `deftransformp`
    - Adjust `defdelegate`, `defguard`, and `defguardp` definitions
    - Include documentation definitions from `@deprecated`
    - Adjust documentation definitions to be more resilient

--- languages/elixir/textobjects.scm
+++ languages/elixir/textobjects.scm
@@ -2,17 +2,13 @@
 (call
   target: ((identifier) @_identifier
     (#any-of? @_identifier "defmodule" "defprotocol" "defimpl"))
-  (do_block
-    "do"
-    (_)* @class.inside
-    "end")) @class.around
+  (do_block "do" (_)* @class.inside  "end")) @class.around
 
 ; Anonymous function definitions
 (anonymous_function
-  (stab_clause
-    right: (body) @function.inside)) @function.around
+  (stab_clause right: (body) @function.inside)+) @function.around
 
-; Function definitions with `do_block` bodies
+; Function definitions
 (call
   target: ((identifier) @_identifier
     (#any-of? @_identifier
@@ -21,44 +17,53 @@
       "defmacro"
       "defmacrop"
       "defn"
-      "defnp"))
-  (do_block
-    "do"
-    (_)* @function.inside
-    "end")) @function.around
-
-; Function definitions with `keywords` bodies
-(call
-  target: ((identifier) @_identifier
-    (#any-of? @_identifier
-      "def"
-      "defp"
-      "defmacro"
-      "defmacrop"
-      "defn"
-      "defnp"))
+      "defnp"
+      "deftransform"
+      "deftransformp"))
   (arguments
     (_)
     (keywords
       (pair
-        value: (_) @function.inside)))) @function.around
+        key: ((keyword) @_keyword (#eq? @_keyword "do: "))
+        value: (_) @function.inside))?)?
+  (do_block "do" (_)* @function.inside "end")?) @function.around
 
+; Function definitions from delegations
 (call
   target: ((identifier) @_identifier
-    (#any-of? @_identifier "defdelegate" "defguard" "defguardp"))) @function.around
+    (#eq? @_identifier "defdelegate"))
+  (arguments
+    (_)
+    (keywords (pair)* @function.inside)?)?) @function.around
+
+; Guard definitions
+(call
+  target: ((identifier) @_identifier
+    (#any-of? @_identifier "defguard" "defguardp"))
+  (arguments
+    (binary_operator
+      operator: "when"
+      right: (_) @function.inside)?)) @function.around
 
 ; Comment definitions
-(comment) @comment.around
+((comment)+ @comment.around) @comment.inside
 
 ; Documentation definitions
 (unary_operator
   operator: "@"
   operand: (call
     target: ((identifier) @_identifier
-      (#any-of? @_identifier "moduledoc" "typedoc" "shortdoc" "doc"))
+      (#any-of? @_identifier
+        "deprecated"
+        "moduledoc"
+        "typedoc"
+        "shortdoc"
+        "doc"))
     (arguments
       [
-        (keywords) @comment.inside
-        (string
-          (quoted_content) @comment.inside)
+        (string (quoted_content) @comment.inside)
+        (charlist (quoted_content) @comment.inside)
+        (sigil (quoted_content) @comment.inside)
+        (keywords (pair)* @comment.inside)
+        (_) @comment.inside
       ]))) @comment.around
