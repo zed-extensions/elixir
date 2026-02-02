9a859759a34144d8352589349133a6499a2e6529
AltCode <12354672+AlternateRT@users.noreply.github.com>
2026-02-02 17:47:28 +0100

    Improve Elixir brackets and indents
    - Scopes all brackets so that detection only happens where it should
    - Adds detection for interpolation delimiters
    - Adds detection for all sigil delimiters
    - Adds detection for all charlist delimiters
    - Adds auto-indents to lists, blocks, tuples, maps, and parentheses

--- languages/elixir/brackets.scm
+++ languages/elixir/brackets.scm
@@ -1,8 +1,44 @@
-("(" @open ")" @close)
-("[" @open "]" @close)
-("{" @open "}" @close)
-(("<<" @open ">>" @close) (#set! rainbow.exclude))
-(("\"\"\"" @open "\"\"\"" @close) (#set! rainbow.exclude))
-(("\"" @open "\"" @close) (#set! rainbow.exclude))
-(("'" @open "'" @close) (#set! rainbow.exclude))
-("do" @open "end" @close)
+(tuple
+  "{" @open
+  "}" @close)
+
+(list
+  "[" @open
+  "]" @close)
+
+(block
+  "(" @open
+  ")" @close)
+
+(map
+  "{" @open
+  "}" @close)
+
+(do_block
+  "do" @open
+  "end" @close)
+
+(interpolation
+  "#{" @open
+  "}" @close
+  (#set! rainbow.exclude))
+
+(bitstring
+  "<<" @open
+  ">>" @close
+  (#set! rainbow.exclude))
+
+(string
+  quoted_start: _ @open
+  quoted_end: _ @close
+  (#set! rainbow.exclude))
+
+(charlist
+  quoted_start: _ @open
+  quoted_end: _ @close
+  (#set! rainbow.exclude))
+
+(sigil
+  quoted_start: _ @open
+  quoted_end: _ @close
+  (#set! rainbow.exclude))
--- languages/elixir/indents.scm
+++ languages/elixir/indents.scm
@@ -1,15 +1,20 @@
 [
+  (tuple "}" @end)
+  (list "]" @end)
+  (block ")" @end)
+  (map "}" @end)
+  (bitstring ">>" @end)
+  (do_block "end" @end)
+  (anonymous_function "end" @end)
+  (call (arguments ")" @end))
+] @indent
+
+; These have no end delimiter that can be captured,
+; as they are usually nested inside `do_block` nodes
+[
   (after_block)
-  (anonymous_function)
   (catch_block)
-  (do_block)
   (else_block)
   (rescue_block)
   (stab_clause)
-  (bitstring)
 ] @indent
-
-[
-  ">>"
-  "end"
-] @outdent
