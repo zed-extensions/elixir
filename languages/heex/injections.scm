c37cdc210425e9ed1074e127eb721e52f0bbad22
AltCode <altcode@vivaldi.net>
2026-02-07 17:10:09 +0100

    Adjust HEEx config, brackets, and injections
    - Corrects naming of HEEx language
    - Adds other suffixes that should be recognised as HEEx files
    - Enforces a tab size of 2 by default (the formatter enforces this too)
    - Excludes bracket colorization for HTML tags
    - Adds injections for `<script>` and `<style>` tags, and comments
    - Cleans up injections for Elixir (and annotates what each one does)
    
    Injections copied from phoenixframework/tree-sitter-heex#44

--- languages/heex/injections.scm
+++ languages/heex/injections.scm
@@ -1,13 +1,53 @@
-(
-  (directive
-    [
-      (partial_expression_value)
-      (expression_value)
-      (ending_expression_value)
-    ] @injection.content)
+; Directives are standalone tags like '<%= @x %>'
+;
+; `partial_expression_value`s are Elixir code that is part of an expression that
+; spans multiple directive nodes, so they must be combined. For example:
+;     <%= if true do %>
+;       <p>hello, tree-sitter!</p>
+;     <% end %>
+((directive
+  [
+    (partial_expression_value)
+    (ending_expression_value)
+  ] @injection.content)
   (#set! injection.language "elixir")
-  (#set! injection.combined)
-)
+  (#set! injection.include-children)
+  (#set! injection.combined))
 
+; Regular `expression_value`s do not need to be combined
+((directive (expression_value) @injection.content)
+  (#set! injection.language "elixir"))
+
+; Expressions live within HTML tags, and do not need to be combined
+;     <link href={ Routes.static_path(..) } />
+; Note that we include children because `expression_value` may consist
+; of multiple nodes when the value contains { and }
 ((expression (expression_value) @injection.content)
- (#set! injection.language "elixir"))
+  (#set! injection.language "elixir")
+  (#set! injection.include-children))
+
+; Syntax highlight for <script> tags
+((tag
+  (start_tag
+    (tag_name) @tag_name (#eq? @tag_name "script"))
+  (text)
+  (end_tag)) @injection.content
+  (#offset! @injection.content 1 0 0 -9)
+  (#set! injection.language "javascript")
+  (#set! injection.include-children)
+  (#set! injection.combined))
+
+; Syntax highlight for <style> tags
+((tag
+  (start_tag
+    (tag_name) @tag_name (#eq? @tag_name "style"))
+  (text)
+  (end_tag)) @injection.content
+  (#offset! @injection.content 1 0 0 -9)
+  (#set! injection.language "css")
+  (#set! injection.include-children)
+  (#set! injection.combined))
+
+; Comment parsing languages support
+((comment) @injection.content
+  (#set! injection.language "comment"))
