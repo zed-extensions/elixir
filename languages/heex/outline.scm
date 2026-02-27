f5a107b5c3b0205c3aebb86b35cec8c911ec93c1
AltCode <altcode@vivaldi.net>
2026-02-27 11:38:03 +0100

    Add HEEx indents and outline
    These queries should match those used by HTML

---
+++ languages/heex/outline.scm
@@ -0,0 +1,13 @@
+(comment) @annotation
+
+[
+  (tag
+    (start_tag
+      (tag_name) @name))
+  (slot
+    (start_slot
+      (slot_name) @name))
+  (component
+    (start_component
+      (component_name) @name))
+] @item
