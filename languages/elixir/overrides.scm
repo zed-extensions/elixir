f785135c3ec38bc5cdf1f96282c5fd7fce86d04a
AltCode <altcode@vivaldi.net>
2026-03-02 00:15:57 +0100

    Add more syntax overrides
    - Adds `!`, `?`, and `@` as word characters for identifiers
      - Does the same for HEEx, but only for the first two
    - Adds `\` as a word character for char literals
    - Adds `&` as a word character for parameter placeholders within
      anonymous function captures
    - Disables bracket auto-closing within sigils using those brackets as
      delimiters
    - Fixes Elixir completion query characters for Tailwind
    - Fixes range inclusivity for comments override in HEEx

--- languages/elixir/overrides.scm
+++ languages/elixir/overrides.scm
@@ -4,3 +4,49 @@
   (string)
   (charlist)
 ] @string
+
+[
+  (quoted_atom)
+  (quoted_keyword)
+] @quoted_atom
+
+[
+  (identifier)
+  (atom)
+  (keyword)
+  (unary_operator
+    operator: "@")
+] @identifier
+
+(char) @char
+
+(unary_operator
+  operator: "&"
+  operand: (integer)) @capture
+
+(sigil
+  quoted_start: "{"
+  quoted_end: "}") @sigil_curly
+
+(sigil
+  quoted_start: "["
+  quoted_end: "]") @sigil_square
+
+(sigil
+  quoted_start: "("
+  quoted_end: ")") @sigil_round
+
+[
+  (sigil
+  quoted_start: "\"\"\""
+  quoted_end: "\"\"\"")
+  (sigil
+  quoted_start: "\""
+  quoted_end: "\"")
+  (sigil
+  quoted_start: "'''"
+  quoted_end: "'''")
+  (sigil
+  quoted_start: "'"
+  quoted_end: "'")
+] @sigil_string
