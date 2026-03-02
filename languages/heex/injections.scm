6ddfba79e7585aca4e2b53d468cbd2fe4fc3754a
AltCode <altcode@vivaldi.net>
2026-03-02 23:50:04 +0100

    Adjust HEEx injections (#75)
    - Adds injections for `style="..."` and `onEVENT="..."` attributes, and
    comments
    - Cleans up injections for Elixir (and annotates what each one does)
    
    Injections copied from phoenixframework/tree-sitter-heex#44 and
    zed-industries/zed#23659

--- languages/heex/injections.scm
+++ languages/heex/injections.scm
@@ -1,12 +1,50 @@
-((directive
+; Expressions are HTML-aware interpolations of Elixir code like
+; `<link href={ Routes.static_path(..) } />`
+(expression
+  (expression_value) @injection.content
+  (#set! injection.language "elixir"))
+
+; Directives are standalone tags like '<%= @x %>'
+;
+; Directives with `partial_expression_value` and `ending_expression_value`
+; nodes are Elixir code that is part of an expression that spans multiple
+; `directive` nodes, so they must be combined. For example:
+;     <%= if true do %>
+;       <p>hello, tree-sitter!</p>
+;     <% end %>
+(directive
   [
     (partial_expression_value)
-    (expression_value)
     (ending_expression_value)
-  ] @injection.content)
+  ] @injection.content
   (#set! injection.language "elixir")
   (#set! injection.combined))
 
-((expression
-  (expression_value) @injection.content)
+; Directives with `expression_value` nodes do not need to be combined.
+; For example:
+;     <body>
+;       <%= @inner_content %>
+;     </body>
+(directive
+  (expression_value) @injection.content
   (#set! injection.language "elixir"))
+
+; Syntax highlight for `style="..."` attributes
+(attribute
+  (attribute_name) @_attribute_name
+  (quoted_attribute_value
+    (attribute_value) @injection.content)
+  (#eq? @_attribute_name "style")
+  (#set! injection.language "css"))
+
+; Syntax highlight for `onEVENT="..."` attributes
+(attribute
+  (attribute_name) @_attribute_name
+  (quoted_attribute_value
+    (attribute_value) @injection.content)
+  (#match? @_attribute_name "^on[a-z]+$")
+  (#set! injection.language "javascript"))
+
+; Comment parsing languages support
+((comment) @injection.content
+  (#set! injection.language "comment"))
